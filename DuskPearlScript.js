2e411fb76bb4b59df9f6f81648c881abf76be8e9
霊音・永旻 <109428665+eimirein@users.noreply.github.com>
2022-08-03 13:35:59 +0900

    Add files via upload

--- DuskPearlScript.js
+++ DuskPearlScript.js
@@ -37,6 +37,30 @@
 	}
 	// Set/change a global CSS variable
 	function pattern(CSSVar, val) { document.documentElement.style.setProperty(CSSVar, val) }
+	// Track a mouse X/Y position relative to element's boundBox or radius >> [id.mx/id.my/id.mr]
+	// document.getElementById(___).addEventListener("mousemove", track, false)
+	function track(event) {
+		var boundBox = event.target.getBoundingClientRect();
+		var x1 = event.clientX - boundBox.left;
+		var x2 = event.clientX - boundBox.right;
+		var y1 = event.clientY - boundBox.top;
+		var y2 = event.clientY - boundBox.bottom;
+		let min = 0, max = 100
+		var id = event.target.id
+		document.getElementById(id).mx =
+			Math.max( min, Math.min( max, Math.round( max * Math.abs(x1 / boundBox.width) ) ) )
+		document.getElementById(id).my =
+			Math.max( min, Math.min( max, Math.round( max * Math.abs(y2 / boundBox.height) ) ) )
+		document.getElementById(id).mr =
+			Math.max( // Knowing no math? Let's draw diagonals!
+				Math.max( min, Math.min( max, Math.round( max * Math.abs(x1 / boundBox.width) )-50 )*2 ),
+				Math.max( min, Math.min( max, Math.round( max * Math.abs(x2 / boundBox.width) )-50 )*2 ),
+				Math.max( min, Math.min( max, Math.round( max * Math.abs(y1 / boundBox.height) )-50 )*2 ),
+				Math.max( min, Math.min( max, Math.round( max * Math.abs(y2 / boundBox.height) )-50 )*2 ),
+				Math.max( min, Math.min( max, Math.round( max * Math.abs( (x1-y1)*1.41 / boundBox.width) ) ) ),
+				Math.max( min, Math.min( max, Math.round( max * Math.abs( (x1+y2)*1.41 / boundBox.width) ) ) )
+			)
+	}
 	
 	// Themes(?)
 	document.getElementById('themeDark').onmouseover = function() {
@@ -48,14 +72,23 @@
 		pattern('--textColor', '#000')
 	}
 	
-	// Active style switches
+	// Active style switches (may include optional components)
 	document.getElementById('core').onmouseover = function() {
 		document.getElementById('themeDark').style.visibility = 'visible'
 		document.getElementById('themeBright').style.visibility = 'visible'
+		document.getElementById('brightnessCtrl').style.visibility = 'visible'
 	}
 	document.getElementById('core').onmouseout = function() {
 		document.getElementById('themeDark').style.visibility = 'hidden'
 		document.getElementById('themeBright').style.visibility = 'hidden'
+		document.getElementById('brightnessCtrl').style.visibility = 'hidden'
+	}
+	
+	// Functions for optional components
+	document.getElementById('brightnessCtrl').addEventListener("mousemove", track, false)
+	document.getElementById('brightnessCtrl').onclick = function() {
+		document.getElementById('aboutMe').style.background =
+			'#000000'+(10+Math.round(document.getElementById('brightnessCtrl').mr*0.89))
 	}
 	
 	// Decorative: Showcase/debug functions
